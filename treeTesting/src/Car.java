282910f24b9d623be97d3c59ddec833b0f9afb94
Faniz Eliasvand <91394662+fanizeliasvand@users.noreply.github.com>
2022-03-05 00:27:41 -0500

    Add files via upload

---
+++ treeTesting/src/Car.java
@@ -0,0 +1,63 @@
+
+//“On my honor, I have neither received nor given any unauthorized assistance on Final Exam." Faniz Eliasvand 
+public class Car implements Comparable<Car> {
+
+   private String Description, Owner;
+   private String VIN;
+
+   public Car(String description, String owner, String vin) {
+       Description = description;
+       Owner = owner;
+       VIN = vin;
+   }
+   public String getDescription() {
+	   
+	   
+	   
+	   
+       return Description;
+}
+   public String getOwner() {
+	   
+	   
+	   
+	   
+       return Owner;
+   }
+   public String getVIN() {
+	   
+	   
+	   
+       return VIN;
+   }
+
+   public String toString()
+           {
+               return Owner + ", " + Description + " " + VIN + "\t";
+           }
+
+   public boolean equals(Object other) {
+       return (Owner.equals(((Car)other).getOwner()) &&
+               Description.equals(((Car)other).getDescription()) &&
+               (VIN == (((Car)other).getVIN())));
+   }
+
+   public int hashCode() {
+       return VIN.hashCode();
+   }
+
+
+   public int compareTo(Car car) {
+       int result;
+
+       if (Owner.equals(car.getOwner()))
+           result = Owner.compareTo(car.getOwner());
+       else
+           result = Description.compareTo(car.getDescription());
+
+       return result;
+   }
+
+       
+
+}
---
+++ treeTesting/src/TreeTest.java
@@ -0,0 +1,54 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+//“On my honor, I have neither received nor given any unauthorized assistance on Final Exam." Faniz Eliasvand 
+
+
+
+
+public class TreeTest implements Comparator<Car> {
+
+public static void main(String[] args) {
+
+	Tree<Car> tree = new Tree<Car>();
+
+tree.insertNode(new Car("453B55","2007 Porsche","Mary Jane"));
+
+tree.insertNode(new Car("123T56", "2021 caddilac","Eliazabeth Wood"));
+
+tree.insertNode(new Car("247H54", "New Yorker","Simpson Grant"));
+
+tree.insertNode(new Car("237H54", "1998 Grand Jeep","Dan Jacobson"));
+
+tree.insertNode(new Car("123F44", "2012 Lexus","Faniz Eliasvand "));
+
+tree.insertNode(new Car("121A69", "2015 Mercades Benz", "Kevin Karma"));
+
+
+System.out.print("Preorder traversal");
+System.out.println();
+tree.preorderTraversal();
+System.out.println();
+System.out.println();
+
+System.out.print("Inorder traversal");
+System.out.println();
+tree.inorderTraversal();
+System.out.println();
+System.out.println();
+
+System.out.print("Postorder traversal");
+System.out.println();
+tree.postorderTraversal();
+System.out.println();
+System.out.println();
+
+}
+
+@Override
+public int compare(Car o1, Car o2) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+}
